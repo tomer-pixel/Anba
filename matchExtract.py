65c9a8d3853dbf311eb1d647a529acd9e0001e7b
tomer-pixel <83169357+tomer-pixel@users.noreply.github.com>
2023-04-14 19:15:47 +0300

    Add files via upload

--- matchExtract.py
+++ matchExtract.py
@@ -1,10 +1,25 @@
 import mysql.connector
 import pandas as pd
-from nba_api.stats.endpoints import boxscoretraditionalv2
-from nba_api.stats.endpoints import boxscoresummaryv2
+from nba_api.stats.endpoints import scoreboardv2
+from nba_api.stats.endpoints import playbyplayv2
 from nba_api.stats.endpoints import leaguegamefinder
-
-    #connecting to the database:
+from nba_api.stats.endpoints import boxscoretraditionalv2
+from nba_api.stats.endpoints import scoreboardv2
+#connecting to the database:
+class Match:
+    #creating an object of a Match to insert easily
+    def __init__(self, team1, team2, score1, score2, season, game_id):
+        setattr(self, 'team1', team1)
+        setattr(self, 'team2', team2)
+        setattr(self, 'score1', score1)
+        setattr(self, 'score2', score2)
+        setattr(self, 'season', season)
+        setattr(self, 'game_id', game_id)
+    def toTupple(match):
+        object = (match.team1, match.team2, match.score1, match.score2, match.season, match.game_id)
+        print(type(object))
+        return object
+        print(type(object))
 conn = mysql.connector.connect(
 host = "localhost",
 user = "root",
@@ -13,8 +28,8 @@ database = "games"
 )
 cursor = conn.cursor()
 
-#receives a game id and uploads all stats from that game to the statistics table in the database.
-def extractGame(game_id):
+#receives a game id and uploads all basic info  from that game to the matches table in the database.
+def extractGame(game_id, season):
     conn = mysql.connector.connect(
     host = "localhost",
     user = "root",
@@ -22,43 +37,44 @@ def extractGame(game_id):
     database = "games"
     )
     cursor = conn.cursor()
-    boxscore = boxscoretraditionalv2.BoxScoreTraditionalV2(game_id = game_id)
-    player_stats = boxscore.player_stats.get_data_frame()
-    pd_stats = pd.DataFrame(player_stats)
-    #there are 2 tables that are needed to be filled: matches and statistics.
-    #first ill deal with the matches table
-    team1 = pd_stats['TEAM_ID'].iloc[0]
-    team2 = pd_stats['TEAM_ID'].iloc[1]
-    score1 = pd_stats['PTS'].iloc[0]
-    for i in range(len(pd_stats)):
-        team2 = pd_stats['TEAM_ID'].iloc[i]
-        if(team2 != team1):
-            score2 = pd_stats['PTS'].iloc[i]
-            break
-    boxscore_summary = boxscoresummaryv2.BoxScoreSummaryV2(game_id)
-    game_info = boxscore_summary.game_summary.get_data_frame()
-    season = game_info['SEASON'].values
-    season = season[0]
-    print(game_id)
-    #this is the basic information that every nba game will have, no matter who plays
-    sql = "INSERT IGNORE INTO matches(TeamId1, TeamId2, TeamId1Score, TeamId2Score, Season, MatchId) VALUES(%s, %s, %s, %s, %s, %s)"
-    cursor.execute(sql, (str(team1), str(team2), str(score1), str(score2), str(season), str(game_id)))
-    conn.commit()
-cursor.close()
-conn.close()    
+    # Retrieve the box score data for the specified game ID
+    bs = boxscoretraditionalv2.BoxScoreTraditionalV2(game_id)
+    team_stats = bs.team_stats.get_data_frame()
+    # Extract the DataFrame containing the final score information
+    score_df = bs.get_data_frames()[1]
 
-def extractSeasongames(season):
-    gamefinder = leaguegamefinder.LeagueGameFinder()
-    # The first DataFrame of those returned is what we want.
-    games = gamefinder.get_data_frames()[0]
-    wanted_games = games[games.SEASON_ID.str[-4:] == str(season)]
-    game_ids = wanted_games['GAME_ID'].values
-    unique_games = []
-    for id in game_ids:
-        if (id not in unique_games):
-            unique_games.append(id)
-    for id in unique_games:
-        extractGame(str(id))
+    # Extract the final score for each team
+    away_score = score_df.iloc[0]['PTS']
+    home_score = score_df.iloc[1]['PTS']
 
-    
-extractSeasongames('2020')
+    team_ids = team_stats['TEAM_ID']
+    visitor_team = team_ids[0]
+    home_team = team_ids[1]
+
+    team1 = home_team
+    team2 = visitor_team
+    score1 = home_score
+    score2 = away_score
+    if(team1 == None or team2 == None or score1 == None or score2 == None or season == None):
+        print("fuck")
+    else:
+        match_object = Match(int(team1), int(team2), int(score1), int(score2), str(season), str(game_id))
+        match_object = Match.toTupple(match_object)
+        #this is the basic information that every nba game will have, no matter who plays
+        sql = """INSERT IGNORE INTO matches
+        (TeamId1, TeamId2, TeamId1Score, TeamId2Score, Season, MatchId) 
+        VALUES(%s, %s, %s, %s, %s, %s)"""
+        print(game_id)
+        cursor.execute(sql, match_object)
+        conn.commit()
+#recieves a season and inserts all games from that particular season
+def extractSeasonMatches(season):
+    game_finder = leaguegamefinder.LeagueGameFinder(season_nullable = season)
+    games = game_finder.get_data_frames()[0]
+    game_ids = games['GAME_ID'].tolist()
+    print(len(game_ids))
+    for game in game_ids:
+        extractGame(str(game), str(season))
+
+extractSeasonMatches('2022')
+
--- statsExtract.py
+++ statsExtract.py
@@ -1,7 +1,12 @@
 from nba_api.stats.endpoints import boxscoretraditionalv2
 import mysql.connector
 import pandas as pd
-
+from nba_api.stats.endpoints import LeagueGameFinder
+from nba_api.stats.endpoints import boxscoresummaryv2
+def nanToNull(value):
+    if(value == float("NaN")):
+        value = None
+    return value
 #recieves a game id and inserts stats for all players in that game into the database. 
 def extractAllGameStats(game_id):
     #connect to database:
@@ -14,30 +19,45 @@ def extractAllGameStats(game_id):
     #there are 2 teams and roughly 12 players per team who played for some ammount of time in the game.
     #EACH ROW represents a player in that specific game
     df = pd.DataFrame(player_stats)
-    for player in df:
-        player_id = player['PLAYER_ID']
-        position = player['START_POSITION']
-        minutes_played = player['MIN']
-        field_goals_made = player['FGM']
-        field_goals_attempted = player['FGA']
-        three_pointers_made = player['FG3M']
-        three_pointers_attempted = player['FG3A']
-        free_throws_made = player['FTM']
-        free_throws_attempted = player['FTA']
-        offensive_rebound = player['OREB']
-        deffensive_rebound = player['DREB']
-        assists = player['AST']
-        steals = player['STL']
-        blocks = player['BLK']
-        turnovers = player['TO']
-        power_fouls = player['PF']
-        all_points = player['PTS']
-        plus_minus = player['PLUS_MINUS']
+    for player in df.index:
+        player_id = df['PLAYER_ID'].iloc[player]
+        position = df['START_POSITION'].iloc[player]
+        minutes_played = df['MIN'].iloc[player]
+        field_goals_made = df['FGM'].iloc[player]
+        field_goals_attempted = df['FGA'].iloc[player]
+        three_pointers_made = df['FG3M'].iloc[player]
+        three_pointers_attempted = df['FG3A'].iloc[player]
+        free_throws_made = df['FTM'].iloc[player]
+        free_throws_attempted = df['FTA'].iloc[player]
+        offensive_rebound = df['OREB'].iloc[player]
+        deffensive_rebound = df['DREB'].iloc[player]
+        assists = df['AST'].iloc[player]
+        steals = df['STL'].iloc[player]
+        blocks = df['BLK'].iloc[player]
+        turnovers = df['TO'].iloc[player]
+        power_fouls = df['PF'].iloc[player]
+        all_points = df['PTS'].iloc[player]
+        plus_minus = df['PLUS_MINUS'].iloc[player]
+
         #now that we have the data we can insert it into the database
         sql = """INSERT IGNORE into statistics
         (gameId, playerId, points, pos, minutes_played, fgm, fga, tpm, tpa, ftm, fta, offRebound, defRebound, assists, persFouls, steals, turnovers, blocks, plusMinus)
-        VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
+        VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
         """
-        cursor.execute(sql, (game_id, player_id, all_points, position, minutes_played, field_goals_made, field_goals_attempted, three_pointers_made, three_pointers_attempted, free_throws_made, free_throws_attempted, offensive_rebound, deffensive_rebound, assists, power_fouls, steals, turnovers, blocks, plus_minus))
-        
-extractAllGameStats('0022000408')
+        if(minutes_played != None):
+            cursor.execute(sql, (int(game_id), int(player_id), int(all_points), str(position), minutes_played, int(field_goals_made), int(field_goals_attempted), int(three_pointers_made), int(three_pointers_attempted), int(free_throws_made), int(free_throws_attempted), int(offensive_rebound), int(deffensive_rebound), int(assists), int(power_fouls), int(steals), int(turnovers), float(blocks), float(plus_minus)))
+            conn.commit()
+    cursor.close()
+    conn.close()  
+
+def extractSeasonGames(season):
+    game_finder = LeagueGameFinder(season_nullable=season)
+    games = game_finder.get_data_frames()[0]
+    game_ids = games['GAME_ID'].tolist()
+    print(len(game_ids))
+    game_ids.reverse()
+    for game in game_ids:
+        extractAllGameStats(str(game))
+        print(game_ids.index(game))
+
+extractSeasonGames('2013')
